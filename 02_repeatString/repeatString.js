3333e07a388dab0a987646d8c952a82bfb7a306e
kevin <wu4422@gmail.com>
2024-01-12 00:08:31 -0800

    #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
 
+const repeatString = function(repeatedWord, repeatCount) {
+    let myString = "";
+    if (repeatCount < 0){
+        return 'ERROR';
+    }
+    else{
+        for (let i = 0; i < repeatCount; i++){
+            myString += repeatedWord;
+        }
+        return myString;
+    }
 };
 
 // Do not edit below this line
